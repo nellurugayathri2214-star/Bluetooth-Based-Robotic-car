5e53f440ad751f42498f81fdac9222b9478d2fb0
Nelluru Gayathri <nellurugayathri2214@gmail.com>
2025-12-11 13:28:28 +0530

    Add files via upload

---
+++ uart0header.h
@@ -0,0 +1,10 @@
+
+void UART0_config(void);
+void UART0_TX(unsigned char);
+unsigned char UART0_RX(void);
+void UART0_TXstr(unsigned char *);
+void UART0_RXstr(unsigned char *,int);
+void LCD_title(void);
+void PWM_command(unsigned char);
+
+
